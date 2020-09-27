66dbcc1203158f22cc93a540a6241b3936a3f6e6
Sumanth <50363635+SumanthKankipati@users.noreply.github.com>
2020-09-27 13:56:19 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,46 @@
+const main = {
+  name: 'Sumanth Kankipati',
+  mail: 'ksumanth8696@gmail.com',
+  img: 'sumanth.jpg',
+
+  role: ['Machine Learning Enthusiast'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+  
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/sumanthkankipati'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/sumanth-kankipati-ab910a169/'
+    },
+    {
+      name: 'Facebook',
+      iconName: 'logo-facebook',
+      link: 'https://facebook.com/sumanthkankipati'
+    },
+    {
+    name: 'Instagram',
+      iconName: 'logo-instagram',
+      link: 'https://instagram.com/sumanthh_'
+    },
+    
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/ksumanth8696'
+    },
+    
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'ksumanth8696@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+  ]
+};
